bdd56967f444cb0fa148bd8be3b6b6d01154e32f
laraduerlich <lara.duerlich@t-online.com>
2024-12-02 14:42:33 +0100

    add Codierung: ProductRepo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,20 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        Product organge = new Product(1,"Orange");
+        Product apple = new Product(2,"Apple");
+        Product banana = new Product(3,"Banana");
 
+        ProductRepo productRepo = new ProductRepo();
+
+        productRepo.addProduct(organge);
+        productRepo.addProduct(apple);
+        productRepo.addProduct(banana);
+
+        productRepo.printAllProducts();
+        productRepo.removeProduct(organge);
+        productRepo.printAllProducts();
+
+        System.out.println("-".repeat(20));
     }
 }
---
+++ src/main/java/org/example/ProductRepo.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ProductRepo {
+    List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public void printAllProducts() {
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+
+    public void printProduct(Product product) {
+        System.out.println(product);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductRepo that = (ProductRepo) o;
+        return Objects.equals(products, that.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(products);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductRepo{" +
+                "products=" + products +
+                '}';
+    }
+}
